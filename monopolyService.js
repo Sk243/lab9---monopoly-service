015b96d164026f118065f6397bd3c10f18b4b779
Sk243 <115191814+Sk243@users.noreply.github.com>
2024-11-20 16:12:33 -0500

    Update monopolyService.js

--- monopolyService.js
+++ monopolyService.js
@@ -35,7 +35,7 @@ const db = pgp({
   database: process.env.DB_DATABASE,
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
-  add ssl: {
+  ssl: {
     rejectUnauthorized: false,
     },
 });
